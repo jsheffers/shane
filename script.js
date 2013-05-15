acaa3f9f95536d903d9898bdc57142ffc7b58f77
Shane Jeffers <shanejeffers@mac.com>
2013-05-15 17:03:26 -0400

    Cleaned up javascript, removed trailing code.

--- script.js
+++ script.js
@@ -1,46 +1,21 @@
 jQuery(document).ready(function($) {
-	
+	// IPAD CYCLE
 	if( $('#ipadcontent')[0] ) {
 		$('#ipadcontent').cycle({
 			fx: 'scrollLeft',
-			speed:    1000, 
-		    timeout:  5000 // choose your transition type, ex: fade, scrollUp, shuffle, etc...
+			speed:    1000,
+			timeout:  5000 // choose your transition type, ex: fade, scrollUp, shuffle, etc...
 		});
 	}
-	
+
+	// CHECKBOX ON CONTACT PAGE
 	$('.form-checkbox').click(function(){
 		$(this).toggleClass('checks');
 		$(this).parent().toggleClass('parent');
 	});
-	
-	$('#tothetop').localScroll();
-	
 	$('#webform-component-select-project-type label:first').addClass('first-label');
 
-
-	$('#webform-component-select-project-type').wrap('<div id="ptype" />');
-
-
-
-    var msie6 = $.browser == 'msie' && $.browser.version < 7;
-  
-	  if (!msie6) {
-	    var top = $('#webform-component-select-project-type').offset().top - parseFloat($('#webform-component-select-project-type').css('margin-top').replace(/auto/, 0));
-	    $(window).scroll(function (event) {
-	      // what the y position of the scroll is
-	      var y = $(this).scrollTop();
-	      
-	      // whether that's below the form
-	      if (y >= top) {
-	        // if so, ad the fixed class
-	        $('#webform-component-select-project-type').addClass('fixed');
-	      } else {
-	        // otherwise remove it
-	        $('#webform-component-select-project-type').removeClass('fixed');
-	      }
-	    });
-	  }  
-
-
+	// ANIMATED SCROLL
+	$('#tothetop').localScroll();
 });
 
