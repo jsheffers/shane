362edcd8365e343152baee5bec2709fe49dcdb87
Shane Jeffers <shanejeffers@mac.com>
2013-07-11 12:48:15 -0400

    Jump to the top

--- js/plugins.js
+++ js/plugins.js
@@ -10,10 +10,9 @@
 		$('.featured-inner .btn-large, .sprites-close, .scroll-inner .btnLarge').click(function(){
 			$('.contact-full').slideToggle();
 		});
-
+		$('.block-menu ul.menu .last a, .block-menu-block ul.menu .last a').attr('href', '#');
 		$('.block-menu ul.menu .last a, .block-menu-block ul.menu .last a').click(function(){
 			$('.contact-full').slideToggle();
-			return false;
 		});
 	});
 })(jQuery);
