5d7d315569f30ba157a8cc1d734a0293bfff20c0
Shane Jeffers <shanejeffers@mac.com>
2013-07-11 11:27:02 -0400

    Working on web form

--- webform-form.tpl.php
+++ webform-form.tpl.php
@@ -1,8 +1,12 @@
 <?php
-foreach ($form["submitted"] as $key => $value) {
-    if ("textfield" == $value["#type"]) {
-        $form["submitted"][$key]['#attributes']["placeholder"] = t($value["#title"]);
-    } 
+foreach ($form['submitted'] as $key => $value) {
+ if (isset($form['submitted'][$key])) {
+   $types = array('textfield', 'webform_email', 'textarea');
+
+   if (isset($value['#type']) && in_array($value['#type'], $types)) {
+     $form['submitted'][$key]['#attributes']['placeholder'] = t($value['#title']);
+   }
+ }
 }
 print drupal_render($form['submitted']);
 print drupal_render_children($form);
