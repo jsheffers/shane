95e8b68eaa0fb9b5c36316b426fb086ce0218ae9
Shane Jeffers <shanejeffers@mac.com>
2013-07-11 11:23:32 -0400

    PHP change

--- webform-form.tpl.php
+++ webform-form.tpl.php
@@ -1,3 +1,4 @@
+<?php
 foreach ($form["submitted"] as $key => $value) {
     if ("textfield" == $value["#type"]) {
         $form["submitted"][$key]['#attributes']["placeholder"] = t($value["#title"]);
