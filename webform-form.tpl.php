abdde5b1b7b0d51ff896827accf60a5bca1c3624
Shane Jeffers <shanejeffers@mac.com>
2013-07-11 11:22:37 -0400

    added web form template file

---
+++ webform-form.tpl.php
@@ -0,0 +1,5 @@
+foreach ($form["submitted"] as $key => $value) {
+    if ("textfield" == $value["#type"]) {
+        $form["submitted"][$key]['#attributes']["placeholder"] = t($value["#title"]);
+    } 
+}
