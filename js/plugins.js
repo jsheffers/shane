b50b4a94bb9e7e8751c99e9e1fda7dd3a85b378f
Shane Jeffers <shanejeffers@mac.com>
2013-07-17 10:03:56 -0400

    Tweaked scrollorama

--- js/plugins.js
+++ js/plugins.js
@@ -14,5 +14,9 @@
 		$('.block-menu ul.menu .last a, .block-menu-block ul.menu .last a').click(function(){
 			$('.contact-full').slideToggle();
 		});
+
+		var controller = $.superscrollorama();
+		// individual element tween examples
+		controller.addTween('.project-3 .project-inner', TweenMax.from( $('.project-3 .project-inner'), .5, {css:{opacity: 0}}));
 	});
 })(jQuery);
