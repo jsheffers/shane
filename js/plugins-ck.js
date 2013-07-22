b9b5dc27e2962f40f8b08ccb3b9a01524851ab0c
Shane Jeffers <shanejeffers@mac.com>
2013-07-22 09:12:46 -0400

    Added in view

--- js/plugins-ck.js
+++ js/plugins-ck.js
@@ -1,1 +1,36 @@
-(function(e){e(function(){e(window).on("scroll",function(){e(".scrolled em").text(e(window).scrollTop())});e(".row:even").addClass("even");e(".row:odd").addClass("odd");e(".featured-inner .btn-large, .sprites-close, .scroll-inner .btnLarge").click(function(){e(".contact-full").slideToggle()});e(".block-menu ul.menu .last a, .block-menu-block ul.menu .last a").attr("href","#");e(".block-menu ul.menu .last a, .block-menu-block ul.menu .last a").click(function(){e(".contact-full").slideToggle()});var t=e.superscrollorama({reverse:!1,triggerAtCenter:!0});t.addTween(".project-3 .project-inner",TweenMax.from(e(".project-3 .project-inner"),.75,{css:{opacity:0}}));t.addTween(".project-4 .project-inner",TweenMax.from(e(".project-4 .project-inner"),.75,{css:{opacity:0}}))})})(jQuery);
+(function ($) {
+	$(function(){
+		$(window).on('scroll', function(){
+			$('.scrolled em').text($(window).scrollTop());
+		});
+
+		$('.row:even').addClass('even');
+		$('.row:odd').addClass('odd');
+
+		$('.featured-inner .btn-large, .sprites-close, .scroll-inner .btnLarge').click(function(){
+			$('.contact-full').slideToggle();
+		});
+		$('.block-menu ul.menu .last a, .block-menu-block ul.menu .last a').attr('href', '#');
+		$('.block-menu ul.menu .last a, .block-menu-block ul.menu .last a').click(function(){
+			$('.contact-full').slideToggle();
+		});
+
+	// 	var controller = $.superscrollorama({
+	// 		reverse: false,
+	// 		triggerAtCenter: true
+	// 	});
+	// 	// individual element tween examples
+	// 	controller.addTween('.project-3 .project-inner', TweenMax.from( $('.project-3 .project-inner'), 0.75, {css:{opacity: 0}}));
+	// 	controller.addTween('.project-4 .project-inner', TweenMax.from( $('.project-4 .project-inner'), 0.75, {css:{opacity: 0}}));
+	// });
+
+	$('.views-row-even.project .featured-image').bind('inview', function (event, visible) {
+	  if (visible == true) {
+	    $(this).addClass('fadeInRightBig animated');
+	  } else {
+	  	$(this).addClass('show');
+	  }
+	});
+
+	
+})(jQuery);
