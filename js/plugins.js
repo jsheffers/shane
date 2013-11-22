235d1ff76d5562323df88f7183a794d6e9199788
Shane Jeffers <shanejeffers@mac.com>
2013-11-22 11:00:09 -0500

    removed slide toggle on contact form

--- js/plugins.js
+++ js/plugins.js
@@ -10,10 +10,10 @@
 		$('.featured-inner .btn-large, .sprites-close, .scroll-inner .btnLarge').click(function(){
 			$('.contact-full').slideToggle();
 		});
-		$('.block-menu ul.menu .last a, .block-menu-block ul.menu .last a').attr('href', '#');
-		$('.block-menu ul.menu .last a, .block-menu-block ul.menu .last a').click(function(){
-			$('.contact-full').slideToggle();
-		});
+		// $('.block-menu ul.menu .last a, .block-menu-block ul.menu .last a').attr('href', '#');
+		// $('.block-menu ul.menu .last a, .block-menu-block ul.menu .last a').click(function(){
+		// 	$('.contact-full').slideToggle();
+		// });
 
 		$('.views-row-even.project-3 .featured-image').bind('inview', function (event, visible) {
 			if (visible === true) {
